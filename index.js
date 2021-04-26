98fc8dcae4ab484f92be440f118d2796ca654fa4
mohammad yaseen <yaseenyahya021@gmail.com>
2021-04-27 00:07:46 +0500

    make it better

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ const server = new ApolloServer({
 });
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/build")));
-  //  app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })}
+   app.get('*', (req, res) => {    res.sendfile(path.join(__dirname = 'client/build/index.html'));  })
 }
 app.use(async (req, res, next) => {
   const accessToken = req.cookies["access-token"];
